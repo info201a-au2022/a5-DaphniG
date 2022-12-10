19bc47bde00d25b2dc8f05986aed321a78d8eb67
Daphni <daphng@uw.edu>
2022-12-09 20:41:00 -0800

    final

--- code/app_server.R
+++ code/app_server.R
@@ -5,10 +5,12 @@ library(dplyr)
 library(plotly)
 library(readr)
 
+
+
 # dataset for CO2
 co2_data <- read_csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv")
 
-shinyServer(function(input,output) {
+server <- function(input,output) {
   output$selectCountry <- renderUI({
     selectInput("country", "Choose a country:", choices = unique(co2_data$country))
     
@@ -24,9 +26,11 @@ shinyServer(function(input,output) {
     
     
     
-    plot_ly(plotData, x = ~year, y = ~gas_co2, type = 'scatter', mode = 'lines',name = 'gas') %>% layout(yaxis = list(title = 'co2 emissions')) %>% 
+    plot_ly(plotData, x = ~year, y = ~gas_co2, type = 'scatter', mode = 'lines',name = 'gas') %>% 
+      layout(yaxis = list(title = 'CO2 Emissions (million tonnes)'), title = "Contributers to CO2 emissions over Time", xaxis = list(title = 'Year'),
+             legend = list(title=list(text='<b> Contributers of CO2 </b>'))) %>% 
       add_trace(y = ~coal_co2, name = 'coal')%>% add_trace(y = ~oil_co2, name = 'oil') %>% 
-      add_trace(y = ~trade_co2, name = 'trade')
+      add_trace(y = ~cement_co2, name = 'cement')
     
     
   })
@@ -34,4 +38,37 @@ shinyServer(function(input,output) {
     linePlot()
     
   })
-})
+}
+
+avg_value <- function(co2_data) {
+  
+  avg <- co2_data %>% filter(year == max(year)) %>% summarise(mean = mean(oil_co2,na.rm=TRUE))
+  return(avg)
+}
+
+val <- avg_value(co2_data)
+
+
+
+#How much has co2 emissions by coal change over the last 11 years
+
+change <- function(co2_data) {
+  big <- co2_data %>% filter(year > 2009)%>% group_by(year) %>% summarise(sum = sum(gas_co2,na.rm=TRUE)) 
+  recent <-big %>% filter(year == max(year)) %>% pull(sum)
+  old <- big %>% filter(year == min(year)) %>% pull(sum)
+  
+  range <- recent - old
+  
+  
+  return(range)
+}
+
+val_3 <- change(co2_data)
+biggest <- function(co2_data){
+  df <- co2_data %>% group_by(year) %>% summarise(sum = sum(coal_co2,na.rm=TRUE)) 
+  most <- df %>% filter (sum == max(sum)) %>% pull(year)
+  return(most)
+  
+}
+val_20<- biggest(co2_data)
+
--- code/app_ui.R
+++ code/app_ui.R
@@ -8,24 +8,68 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
 library(plotly)
+library(readr)
+library(shinythemes)
+
+co2_data <- read_csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv")
+
 
 # Define UI for application that draws a histogram
-navbarPage("A5: Data Applications",
-           tabPanel("Introduction"),
-           tabPanel("Interactive Visulizations",
-                    titlePanel("CO2 Emissions Causes"),
+ui <- navbarPage(theme = shinytheme("darkly"),"A5: Data Applications",
+           tabPanel("Introduction", h1("Overview"),
+                    p("This application will look into the dataset: 
+                    Greenhouse Gas Emissions by Our World in Data. 
+                    The application has an interactive visualization 
+                    that shows the amount of  CO2 emissions by million tonnes 
+                    that are emitted by different factors. Users can select the 
+                    country and the years they want displayed on the line graph. 
+                    The contributors of CO2 emissions the visualization looks at 
+                    are cement, oil, gas and coal. The line graph plots them and 
+                    allows for users to see how the emissions changed over time 
+                    for each country. "),
+                    
+                    p("Looking at the Greenhouse Gas Emissions dataset, 
+                      I decided to look at what the average value of CO2 
+                      emissions emitted by oil was across all the counties 
+                      in the most recent year.  The average value of CO2 
+                      emissions by gas came out to be about"
+                      ,avg_value(co2_data), "million tonnes.", " Next, I looked at which 
+                      year had the highest CO2 emissions by coal. 
+                      It turned out that", biggest(co2_data),"had the highest CO2 
+                      emissions by coal. Finally I looked at how much 
+                      the CO2 emissions by coal changed over the last 11 years. 
+                      CO2 emissions by gas changed about", change(co2_data),
+                      "million tonnes over the past 11 years.") ),
+                   
+
+           tabPanel("Interactive Visulization",
+                    titlePanel("CO2 Emissions Contributers"),
                     
                     # Sidebar with a slider input for selecting countries
                     sidebarLayout(
                       sidebarPanel(
                         uiOutput("selectCountry"),
                         sliderInput("slider1", label = h3("Year"), min = 1850,
-                                    max = 2022, value = 10, sep = ""),
+                                    max = 2022, value = 1850, sep = ""),
+                       
                         
-                        #p("This bar graph depicts the crime & safety of seventeen countries in North, South, and Central America. There is a yellow bar representing the crime rate in each of those countries. On the other hand, there is the green bar representing the safety. The lower the crime, the more safer is to live in that country. By selecting one country at a time through the selecting panel, the bar graph reveals that Venezuela in South America has the most crime rate (83.19) which means that it is 16.84 safe. It is also visible that Panama in Central America has the lowest crime rate (43.61) which means that it is 56.39 safe to live.")
-                        
+          p("This chart was included because it highlights 4 factors that 
+          contribute to CO2 emissions. This chart allows countries to see 
+          how much of each factor is contributing to their CO2 emissions. 
+          Additionally the chart allows viewers to see how much each contributor 
+          increases and decreases over time. Clicking through different countries 
+          we can see that many countries don't have as much data for past years 
+          compared to other countries. Additionally we are able to see that oil, 
+          especially in more recent years, has a higher number of CO2 emissions 
+          compared to the other contributors. However when looking at past years 
+          we can see that is was coal that contributed to CO2 emissions the most. 
+            ")
                       ),
+                      
                       
                       # Show a plot of the generated distribution
                       mainPanel(
